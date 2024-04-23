4c6c5769d5f4b2d2de0984e761f521a72214d809
Kirill Mikheev <65455894+mikkirik@users.noreply.github.com>
2024-04-23 20:41:08 +0300

    Немного косметических улучшений

--- pereval/urls.py
+++ pereval/urls.py
@@ -23,14 +23,6 @@ urlpatterns = [
     path('restapi/', include('restapi.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path(
-        'api/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui'
-    ),
-    path(
-        'api/schema/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc'
-    ),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
